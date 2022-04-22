42935b463174700aa9f6071a26607a3f8b835328
Sandra Ashipala <sajustsmile@live.com>
2022-04-22 19:23:47 +0200

    Update Posts.js

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,3 +1,4 @@
+/* Designed & coded by Sandra Ashipala <https://github.com/sandramsc> */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
